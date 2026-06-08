9dd9302f71a6bff40acf928cf7061312c339493c
Attila135 <magyarosia.25ad@acsjszki.hu>
2026-06-08 16:21:47 +0200

    1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var ora = documemt.getElementById("ora")
+var ora = document.getElementById('ora')
 
-function ido(params) {
+function ido() {
     var datum = new Date();
     ora.innerHTML = datum.getHours() + ":" + datum.getMinutes() + ":" + datum.getSeconds();
     setTimeout(ido, 1000);
